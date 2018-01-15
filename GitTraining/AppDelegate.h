1d5bf8dcde3f2d9b4579bbce0f06c75c09e1e5f3
KentAgent <georgios.apostolidis92@gmail.com>
2018-01-15 22:34:22 +0100

    Initial Commit

---
+++ GitTraining/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitTraining
+//
+//  Created by Georgios on 2018-01-15.
+//  Copyright © 2018 Georgios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
